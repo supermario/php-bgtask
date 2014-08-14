31a871adceb768e1f65d479a3c1adb2b7db1e025
Mario Rogic <hello@mario.net.au>
2014-08-14 19:22:28 +1000

    Re-indent to 2 spaces

--- demo/demo.php
+++ demo/demo.php
@@ -29,31 +29,31 @@ $demoTask = new bgTask('demoTask');
 
 if ($demoTask->status() == bgTask::STATUS_EMPTY) {
 
-    // Task is empty and ready to run a new command!
-    $demoTask->exec($demoTaskCommand);
-    print "We've started a new bgTask! Refresh to query it again and see any output it might have.";
+  // Task is empty and ready to run a new command!
+  $demoTask->exec($demoTaskCommand);
+  print "We've started a new bgTask! Refresh to query it again and see any output it might have.";
 
 } else if ($demoTask->status() == bgTask::STATUS_COMPLETE) {
 
-    // Task has completed
-    print "Our task has completed. Its final output was:";
-    print "<pre>".$demoTask->output()."</pre>";
+  // Task has completed
+  print "Our task has completed. Its final output was:";
+  print "<pre>".$demoTask->output()."</pre>";
 
-    // Once a task has been polled with a result of STATUS_COMPLETE, all temp files for that bgTask are destroyed
-    // This means it's up to you to save the output/exitcode somewhere (i.e. into a database) if you wish to retain it further
-    // In this demo we simply display it to the user, and no history gets kept
+  // Once a task has been polled with a result of STATUS_COMPLETE, all temp files for that bgTask are destroyed
+  // This means it's up to you to save the output/exitcode somewhere (i.e. into a database) if you wish to retain it further
+  // In this demo we simply display it to the user, and no history gets kept
 
 } else if ($demoTask->status() == bgTask::STATUS_ERROR) {
 
-    print "There was an error with our task. It's output was:";
-    print "<pre>".$demoTask->output()."</pre>";
-    print "and it terminated with an exit code of ".$demoTask->exitcode();
+  print "There was an error with our task. It's output was:";
+  print "<pre>".$demoTask->output()."</pre>";
+  print "and it terminated with an exit code of ".$demoTask->exitcode();
 
 } else {
 
-    // Task is already running, print out it's output thus far
-    print "Our task is already running. Here is it's output so far:";
-    print "<pre>".$demoTask->output()."</pre>";
-    print "Continue refreshing to check for futher output and completion.";
+  // Task is already running, print out it's output thus far
+  print "Our task is already running. Here is it's output so far:";
+  print "<pre>".$demoTask->output()."</pre>";
+  print "Continue refreshing to check for futher output and completion.";
 
 }
